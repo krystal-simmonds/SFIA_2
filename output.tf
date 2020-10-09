38124f53f6a75df5c16ade346bb87d8e9ed5b8ce
Krystal <krystalsimm01@me.com>
2020-10-09 16:06:33 +0100

    edited output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "instance_public_DNS" {
+output "jenkins_instance_public_DNS" {
   description = "Public DNS for Jenkins instance"
   value       = aws_instance.jenkins.public_dns
 }
 
-output "instance_public_DNS" {
+output "test_instance_public_DNS" {
   description = "Public DNS for test instance"
   value       = aws_instance.test.public_dns
 }
